f7856bf1b5241c88f09c832a4235536dfb60328b
Emil <macbook@10-18-72-163.ddhcp.uni-oldenburg.de>
2025-06-12 14:46:52 +0200

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="text-4xl font-bold text-green-500"> 
+      <Button>Click me</Button>
     </div>
   );
 }
